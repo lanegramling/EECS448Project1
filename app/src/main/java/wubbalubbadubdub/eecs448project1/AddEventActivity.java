4984ba003b4e930441b18e1e36a3a09e180e02c3
Lane Gramling <lanegramling@gmail.com>
2017-09-13 09:44:24 -0500

    AddEvent Activity

---
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/AddEventActivity.java
@@ -0,0 +1,13 @@
+package wubbalubbadubdub.eecs448project1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddEventActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
