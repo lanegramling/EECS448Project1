e2f1691642853428a7603a3b06a5223b4a9fdecc
dustinbingham <31479630+dustinbingham@users.noreply.github.com>
2017-09-13 10:00:46 -0500

    ViewActivity
    Added the View Activity

---
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/ViewActivity.java
@@ -0,0 +1,13 @@
+package wubbalubbadubdub.eecs448project1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
