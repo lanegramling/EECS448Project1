31d4e9039f794459b969eca30ce0d2aad3b6e2f6
dustinbingham <31479630+dustinbingham@users.noreply.github.com>
2017-09-13 12:46:47 -0500

    Changed AddEventActivity.java
    Added verify and onButtonClick methods

--- app/src/main/java/wubbalubbadubdub/eecs448project1/AddEventActivity.java
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/AddEventActivity.java
@@ -3,7 +3,34 @@ package wubbalubbadubdub.eecs448project1;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * AddEventActivity.java
+ * @author Dustin
+ * @version 1.0
+ * This Class allows the user to create an event and select timeslots for the event created
+ */
 public class AddEventActivity extends Activity {
+
+    private String userName;
+
+    boolean verify() {
+        //conditions for false: eventName not  within parameters, eventDate isn't real
+        //no timeSlots selected
+        return (true); //will be changed later
+    }
+    void onButtonClick() {
+        //will run verify function and check for trueness
+        //if verift returns true, we can push the information given in the AddEventActivity class
+        //to the database
+        if (verify()){
+            //add user info to database
+        }
+        else{
+            //tell user there has been an error and let user fill out text boxes again
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
