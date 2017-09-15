203e4719368861315903593a12ed0323cf124f22
Lane Gramling <lanegramling@gmail.com>
2017-09-14 22:26:34 -0500

    Restructured and finished username validation in SelectUserActivity.java

--- app/src/main/java/wubbalubbadubdub/eecs448project1/SelectUserActivity.java
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/SelectUserActivity.java
@@ -77,15 +77,7 @@ public class SelectUserActivity extends Activity {
         EditText textbox = (EditText) findViewById(R.id.newUsername);
         String name = textbox.getText().toString();
 
-        if (TextUtils.isEmpty(name)) {
-            statusMessage.setText("ERROR: Please input a name for the new user");
-        } else if (!isValidName(name)) {
-            statusMessage.setText("ERROR: Name contains invalid characters");
-        } else if (dbHelper.addUser(name) == -1) { // -1 when failed sql insert
-            statusMessage.setText("ERROR: That Username already exists");
-        } else {
-            statusMessage.setText(name + " was added to the list of users");
-        }
+        if (isValidName(name)) statusMessage.setText(name + " was added to the list of users");
         statusMessage.show();
         populateUsers();
     }
@@ -97,11 +89,17 @@ public class SelectUserActivity extends Activity {
      * @since 1.0
      */
     private boolean isValidName(String name) {
-        // TODO Change this to actual conditions later
-        if (TextUtils.isEmpty(name)) {
+
+        //Conditions for valid username creation
+        if (TextUtils.isEmpty(name)){                    //Empty name
+            statusMessage.setText("ERROR: Please input a name for the new user");
             return false;
-        } else {
-            return true;
-        }//SELECTUSER BRANCH TEST
+        } else if (!name.matches("[a-zA-Z]+")) {    //Non a-z characters in name
+            statusMessage.setText("ERROR: Name contains invalid characters");
+            return false;
+        } else if (dbHelper.addUser(name) == -1) {       //name already exists
+            statusMessage.setText("ERROR: That Username already exists!");
+            return false;
+        }else return true;
     }
 }
