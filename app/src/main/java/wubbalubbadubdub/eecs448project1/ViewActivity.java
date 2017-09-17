1a328124a8ccfaec2989288d66d426a9d7847ab5
Damian Vu <contact@damianvu.com>
2017-09-17 18:06:40 -0500

    Viewing event availability is implemented

--- app/src/main/java/wubbalubbadubdub/eecs448project1/ViewActivity.java
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/ViewActivity.java
@@ -3,6 +3,7 @@ package wubbalubbadubdub.eecs448project1;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -32,6 +33,8 @@ public class ViewActivity extends Activity {
 
     private List<Integer> currentTimeslots;
     private List<Integer> selectedTimeslots;
+
+    private int selectedRow = -1;
 
     private Map<String, String> userSignups;
 
@@ -153,7 +156,62 @@ public class ViewActivity extends Activity {
     }
 
     private void displayEventSignups() {
+        TableLayout layout = (TableLayout) findViewById(R.id.tbLayout);
 
+        TableRow header = new TableRow(this);
+
+
+        TableRow.LayoutParams cellParams = new TableRow.LayoutParams();
+        cellParams.setMargins(10, 5, 10, 5);
+
+        TextView userHeader = new TextView(this);
+        userHeader.setText("User");
+        userHeader.setTextSize(15);
+        userHeader.setTypeface(null, Typeface.BOLD);
+        userHeader.setLayoutParams(cellParams);
+        header.addView(userHeader);
+
+        for (int slot : currentTimeslots) {
+            TextView slotHeader = new TextView(this);
+            slotHeader.setText(HelperMethods.toTime(slot, format));
+            slotHeader.setTextSize(15);
+            slotHeader.setTypeface(null, Typeface.BOLD);
+            slotHeader.setLayoutParams(cellParams);
+            header.addView(slotHeader);
+        }
+
+        header.setBackgroundColor(Color.GRAY);
+
+        layout.addView(header);
+
+        for (Map.Entry<String, String> entry : userSignups.entrySet()) {
+            TableRow signupRow = new TableRow(this);
+
+            TextView username = new TextView(this);
+            username.setPadding(10, 5, 10, 5);
+            username.setText(entry.getKey());
+            signupRow.addView(username);
+            List<Integer> slots = HelperMethods.listifyTimeslotInts(entry.getValue());
+
+            for (int slot : currentTimeslots) {
+                TextView avail = new TextView(this);
+
+                if (slots.contains(Integer.valueOf(slot))) {
+                    // User is signed up for this
+                    avail.setText("AVAILABLE");
+                    avail.setBackgroundColor(GREEN_MAT);
+                } else {
+                    avail.setText("NOT AVAILABLE");
+                    avail.setBackgroundColor(Color.rgb(239, 83, 80));
+                }
+                avail.setPadding(10, 5, 10, 5);
+
+                signupRow.addView(avail);
+            }
+
+            layout.addView(signupRow);
+
+        }
     }
 
     public void saveSelection(View v) {
