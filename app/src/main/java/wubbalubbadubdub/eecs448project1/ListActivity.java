947986ce44557c5031bef64c035e634c125fe43c
Damian <damianvu@ku.edu>
2017-09-16 21:13:24 -0500

    Added event table population (need to have events in the database now...)

--- app/src/main/java/wubbalubbadubdub/eecs448project1/ListActivity.java
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/ListActivity.java
@@ -4,8 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Button;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import wubbalubbadubdub.eecs448project1.data.DatabaseHelper;
+import wubbalubbadubdub.eecs448project1.data.Event;
 
 /**
  * This page list all the activities created by other users.
@@ -17,6 +26,7 @@ import android.widget.Button;
 
 public class ListActivity extends Activity {
     private String currentUser;
+    private DatabaseHelper dbHelper;
 
 
     @Override
@@ -29,8 +39,43 @@ public class ListActivity extends Activity {
         TextView welcomeText = (TextView) findViewById(R.id.tvWelcome);
         welcomeText.setText("Hello " + currentUser + "!");
 
+        dbHelper = new DatabaseHelper(getApplicationContext());
 
 
+        populateEventTable();
+
+    }
+
+    private void populateEventTable() {
+        TableLayout layout = (TableLayout) findViewById(R.id.eventTableLayout);
+
+        ArrayList<Event> events = dbHelper.getAllEvents();
+
+        int numberOfEvents = events.size();
+
+        for (int i = 0; i < numberOfEvents; i++) {
+            TableRow row = new TableRow(this);
+
+            Event workingEvent = events.get(i);
+
+            TextView eventName = new TextView(this);
+            TextView eventCreator = new TextView(this);
+            TextView eventDay = new TextView(this);
+            TextView eventTimeslots = new TextView(this);
+
+            eventName.setText(workingEvent.getName());
+            eventCreator.setText(workingEvent.getCreator());
+            eventDay.setText(workingEvent.getDate());
+            eventTimeslots.setText(workingEvent.getTimeslots());
+
+            row.addView(eventName);
+            row.addView(eventCreator);
+            row.addView(eventDay);
+            row.addView(eventTimeslots);
+
+            layout.addView(row);
+
+        }
     }
 
     public void newActivity(View v) {
