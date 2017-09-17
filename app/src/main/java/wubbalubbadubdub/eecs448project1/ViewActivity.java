4804ae61ef55ff35786abe7440e9a20de3f6c4f2
Damian <damianvu@ku.edu>
2017-09-17 10:19:31 -0500

    Able to move to ViewActivity. Adding functionality now

--- app/src/main/java/wubbalubbadubdub/eecs448project1/ViewActivity.java
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/ViewActivity.java
@@ -1,13 +1,29 @@
 package wubbalubbadubdub.eecs448project1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import wubbalubbadubdub.eecs448project1.data.DatabaseHelper;
+
 public class ViewActivity extends Activity {
+
+    DatabaseHelper dbHelper;
+
+    private int currentID;
+    private String currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        Intent intent = getIntent();
+        currentID = intent.getIntExtra("eventID", -1);
+        currentUser = intent.getStringExtra("currentUser");
+
+        dbHelper = new DatabaseHelper(getApplicationContext());
+
+
     }
 }
