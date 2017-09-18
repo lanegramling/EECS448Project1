dfd4e1011513cf2075eb87bc8ffc52a7dfd3bd5c
Damian Vu <contact@damianvu.com>
2017-09-17 19:41:40 -0500

    Added comments to all data package classes

--- app/src/main/java/wubbalubbadubdub/eecs448project1/data/Event.java
+++ app/src/main/java/wubbalubbadubdub/eecs448project1/data/Event.java
@@ -16,6 +16,14 @@ public class Event implements Comparable<Event> {
     private String creator;
     private String timeslots;
 
+    /**
+     * Constructor for an Event. Events will always be constructed this way.
+     * @param inputID int ID of the event
+     * @param inputDate String date of format MM/DD/YYYY
+     * @param inputName String name of the Event Title
+     * @param inputCreator String name of the Event Creator
+     * @param inputTimeslots String timeslot list of scheduled timeslots
+     */
     public Event(int inputID, String inputDate, String inputName, String inputCreator, String inputTimeslots) {
         id = inputID;
         name = inputName;
@@ -24,6 +32,11 @@ public class Event implements Comparable<Event> {
         timeslots = inputTimeslots;
     }
 
+    /**
+     * Allows events to be compared to each other.
+     * @param otherEvent Event object of event to compare to
+     * @return int < 0 if given event is later than current event. int = 0 if given event is same day as current. int > 0 otherwise
+     */
     public int compareTo(Event otherEvent) {
         int[] currentDate = HelperMethods.getMonthDayYear(date);
         int[] otherDate = HelperMethods.getMonthDayYear(otherEvent.getDate());
@@ -43,20 +56,40 @@ public class Event implements Comparable<Event> {
 
     // Getters
 
+    /**
+     * Getter for the Event ID
+     * @return int ID
+     */
     public int getID() { return id; }
 
+    /**
+     * Getter for the Date String
+     * @return String date of format MM/DD/YYYY
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Getter for the Event Name String
+     * @return String event name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for the Event Creator String
+     * @return String event creator
+     */
     public String getCreator() {
         return creator;
     }
 
+    /**
+     * Getter for the Event Schedule String
+     * @return String of all timeslots for event
+     */
     public String getTimeslots() {
         return timeslots;
     }
